c7606b1f63828b40b1fad98d12a17db5982ff8e1
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2024-12-02 08:09:11 -0300

    Created landing page card

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,10 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const MPLUS1 = localFont({
+  src: "./fonts/MPLUS1.ttf",
+  variable: "--font-m-plus-1",
+  weight: "100 400 700 900",
 });
 
 export const metadata: Metadata = {
@@ -24,12 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="pt-br">
+      <body className={`${MPLUS1.variable} antialiased`}>{children}</body>
     </html>
   );
 }
